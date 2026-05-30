243a65a2df61a5e416d698b653c5e293776f1ead
bmandal05 <bmandal2120@gmail.com>
2026-05-30 23:49:58 +0530

    week 1 - first spring boot server running

---
+++ src/main/java/com/scambaiter/scambaiter/ScambaiterApplication.java
@@ -0,0 +1,13 @@
+package com.scambaiter.scambaiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScambaiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScambaiterApplication.class, args);
+	}
+
+}
